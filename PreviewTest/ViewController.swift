c27de0918f88828cafc67d38fd86c56fc89347ed
Sunny <sunnysideupdowntown@gmail.com>
2024-02-03 09:56:58 +0900

    Initial Commit

---
+++ PreviewTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PreviewTest
+//
+//  Created by YOUNGSUN on 2/3/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
